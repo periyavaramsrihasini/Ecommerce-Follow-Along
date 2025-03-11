dd8ba9b6013968848e3680a3d7ee5d271c5bcff9
unknown <hasiniperiyavaram@gmail>
2025-03-11 13:46:37 +0530

    added

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,10 +1,9 @@
 import Product from "./auth/Product";
-// import Showcase from "./auth/Showcase";
 import Navbar from "./Navbar";
 const Home = () => {
   // Example list of products
   return(
-    <div className="flex flex-col gap-10 bg-gray-700">
+    <div className="flex flex-col gap-2 bg-yellow-400">
       <div>
     <Navbar/>
     </div>
